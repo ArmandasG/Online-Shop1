102ff526117f54b8b8f2f28e5e90fb51c61a3f19
Armandas G <genysarmandas@gmail.com>
2023-05-17 17:18:36 +0300

    added headless UI for tailwind/transition for side

--- src/components/Sandwich.jsx
+++ src/components/Sandwich.jsx
@@ -1,35 +1,48 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { Disclosure, Transition  } from '@headlessui/react'
 
 function Sandwich() {
-  const [myCat, setMyCat] = useState(false);
-  const show = myCat === true ? '' : 'hidden'
+  // const [myCat, setMyCat] = useState(false);
+  // const show = myCat === true ? '' : 'hidden'
 
     function openNav () {
         document.getElementById("myNav").style.width = "100%";
     }
     function closeNav () {
         document.getElementById("myNav").style.width = "0%";
-        setMyCat(false);
     }
+    // console.log('document.getElementById("myNav").style.width ===', document.getElementById("myNav").style.width);
 
-    function displayCat () {
-      myCat === false ?
-      setMyCat(true) : setMyCat(false)
-    }
   return (
   <div>
     <span className='cursor-pointer'><img onClick={openNav} src="/src/icons/Group 6.svg" alt="" /></span>
     <section id='myNav' className='overlay flex-col'>
         <div onClick={closeNav} className='closeBtn cursor-pointer ml-4 container mt-4'><img src="/src/icons/Group 1419.svg" alt="" /></div>
         <div className='mt-2 ml-2 container'>
-        <div onClick={displayCat} className='text-4xl'>Clothes</div>
-        <ul className={`${show ? 'opacity-100' : 'opacity-0'} pl-2 pt-2 transition-all duration-300`}>
+        <Disclosure>
+          {({ open }) => (<>
+        <Disclosure.Button className='text-4xl'>Clothes</Disclosure.Button>
+        <Transition
+        enter="transition duration-300 ease-out"
+        enterFrom="transform scale-y-95 opacity-0"
+        enterTo="transform scale-y-100 opacity-100"
+        leave="transition duration-100 ease-in"
+        leaveFrom="transform scale-y-100 opacity-100"
+        leaveTo="transform scale-y-95 opacity-0"
+      >
+        <Disclosure.Panel>
+        <ul className={`pl-2 pt-2 transition-all duration-300`}>
         <li>Jackets</li>
         <li>Shirts</li>
         <li>Pants</li>
         <li>Shoes</li>
         </ul>
+        </Disclosure.Panel>
+        </Transition>
+        </>
+        )}
+        </Disclosure> 
         <p>blalblab</p>
         </div>
     </section>
--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -1,7 +1,6 @@
 import Cart from "../components/Cart";
 
 function CheckOutPage({onOpen, onClose}) {
-  console.log('onOpen ===', onOpen);
   return (
   <div className='text-2xl'>
     <h4 className="cursor-pointer" onClick={onOpen}>CART(amount)</h4>
