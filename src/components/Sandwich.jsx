7a0f51ffb00d004e7a0f653c3d27a6510f9499cd
Armandas G <genysarmandas@gmail.com>
2023-05-17 18:27:38 +0300

    useState added / acordeon in progress

--- src/components/Sandwich.jsx
+++ src/components/Sandwich.jsx
@@ -3,8 +3,19 @@ import { NavLink } from 'react-router-dom'
 import { Disclosure, Transition  } from '@headlessui/react'
 
 function Sandwich() {
-  // const [myCat, setMyCat] = useState(false);
-  // const show = myCat === true ? '' : 'hidden'
+  const [activeDisclosurePanel, setActiveDisclosurePanel] = useState(null)
+
+  function togglePanels(newPanel) {
+    if (activeDisclosurePanel) {
+      if (activeDisclosurePanel.key !== newPanel.key && activeDisclosurePanel.open) {
+        activeDisclosurePanel.close()
+      }
+    }
+    setActiveDisclosurePanel({
+      ...panel,
+      open: !panel.open
+    })
+  }
 
     function openNav () {
         document.getElementById("myNav").style.width = "100%";
@@ -19,8 +30,8 @@ function Sandwich() {
     <span className='cursor-pointer'><img onClick={openNav} src="/src/icons/Group 6.svg" alt="" /></span>
     <section id='myNav' className='overlay flex-col'>
         <div onClick={closeNav} className='closeBtn cursor-pointer ml-4 container mt-4'><img src="/src/icons/Group 1419.svg" alt="" /></div>
-        <div className='mt-2 ml-2 container'>
-        <Disclosure>
+        <div className='mt-2 ml-2 container flex flex-col items-start'>
+        <Disclosure as='div'>
           {({ open }) => (<>
         <Disclosure.Button className='text-4xl'>Clothes</Disclosure.Button>
         <Transition
@@ -43,7 +54,48 @@ function Sandwich() {
         </>
         )}
         </Disclosure> 
-        <p>blalblab</p>
+        <Disclosure>
+          {({ open }) => (<>
+        <Disclosure.Button className='text-4xl'>Campaigns</Disclosure.Button>
+        <Transition
+        enter="transition duration-300 ease-out"
+        enterFrom="transform scale-y-95 opacity-0"
+        enterTo="transform scale-y-100 opacity-100"
+        leave="transition duration-100 ease-in"
+        leaveFrom="transform scale-y-100 opacity-100"
+        leaveTo="transform scale-y-95 opacity-0"
+      >
+        <Disclosure.Panel>
+        <ul className={`pl-2 pt-2 transition-all duration-300`}>
+        <li>Join us</li>
+        <li>Read more</li>
+        </ul>
+        </Disclosure.Panel>
+        </Transition>
+        </>
+        )}
+        </Disclosure> 
+        <Disclosure>
+          {({ open }) => (<>
+        <Disclosure.Button className='text-4xl'>Press</Disclosure.Button>
+        <Transition
+        enter="transition duration-300 ease-out"
+        enterFrom="transform scale-y-95 opacity-0"
+        enterTo="transform scale-y-100 opacity-100"
+        leave="transition duration-100 ease-in"
+        leaveFrom="transform scale-y-100 opacity-100"
+        leaveTo="transform scale-y-95 opacity-0"
+      >
+        <Disclosure.Panel>
+        <ul className={`pl-2 pt-2 transition-all duration-300`}>
+        <li>What to know ?</li>
+        <li>Where to find us ?</li>
+        </ul>
+        </Disclosure.Panel>
+        </Transition>
+        </>
+        )}
+        </Disclosure> 
         </div>
     </section>
     </div>
