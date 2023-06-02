47686b3326ea0ef3ab64faca476aaf8849a94066
Armandas G <genysarmandas@gmail.com>
2023-06-02 14:59:05 +0300

    snippets for laptop

--- src/assets/selections.js
+++ src/assets/selections.js
@@ -4,15 +4,16 @@ export const color = ["white", "black", "brown", "yellow", "blue", "grey"];
 
 export const size = ["XS", "S", "M", "L", "XL", "XXL"];
 
-export const brand = ["blackIt" , "clothesDown"];
+export const brand = ["blackIt", "clothesDown", "forestUp", "whiteNat"];
 
-export const gender
- = ["men", "women", "uni"];
+export const gender = ["men", "women", "uni"];
+
+export const priceRange = ['0-150']
 
 export const filters = [
-    ["jackets", "pants", "shirts", "shoes"],
-    ["white", "black", "brown", "yellow", "blue", "grey"],
-    ["XS", "S", "M", "L", "XL", "XXL"],
-    ["blackIt", "forestUp", "whiteNat", "clothesDown"],
-    ["men", "women", "uni"]
-]
+  ["jackets", "pants", "shirts", "shoes"],
+  ["white", "black", "brown", "yellow", "blue", "grey"],
+  ["XS", "S", "M", "L", "XL", "XXL"],
+  ["blackIt", "forestUp", "whiteNat", "clothesDown"],
+  ["men", "women", "uni"],
+];
