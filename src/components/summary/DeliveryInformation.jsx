415dd7bff2df4b88a04e28ca08eaec304bd24576
Armandas G <genysarmandas@gmail.com>
2023-08-16 00:57:39 +0300

    Finishing deliveryinformation

--- src/components/summary/DeliveryInformation.jsx
+++ src/components/summary/DeliveryInformation.jsx
@@ -16,6 +16,7 @@ function DeliveryInformation() {
     const formik = useFormik({
         initialValues: {
             email: '',
+            keepMeUpdated: '',
             mobilePhoneNumber: '',
             toggle: false,
             firstName: '',
@@ -23,7 +24,9 @@ function DeliveryInformation() {
             address: '',
             addressExtra: '',
             city: '',
+            location: 'Location',
             postalCode: '',
+            saveInformation: '',
         },
         onSubmit: (values) => {
             console.log('values ===', values);
@@ -37,7 +40,7 @@ function DeliveryInformation() {
     <form className='mt-4 space-y-4 ' onSubmit={formik.handleSubmit}>
       <input className="p-4 border border-black w-full" placeholder='Email' type="email" id='email' value={formik.values.email} onBlur={formik.handleBlur} onChange={formik.handleChange} />
       <label className='flex'>
-      <input name='keepMeUpdated' className="mr-2 w-6 h-6 border-2 border-gray appearance-none checked:bg-black" type="checkbox" />
+      <input value={formik.values.keepMeUpdated} name='keepMeUpdated' className="mr-2 w-6 h-6 border-2 border-gray appearance-none checked:bg-black" type="checkbox" />
       <span className=' text-lg text-gray-500'>Keep me up to date on news and exclusive offers</span>
       </label>
       <input className="p-4 border border-black w-full" placeholder='Mobile phone number' type="phone" id='mobilePhoneNumber' value={formik.values.mobilePhoneNumber} onBlur={formik.handleBlur} onChange={formik.handleChange} />
@@ -51,11 +54,32 @@ function DeliveryInformation() {
         <input className="p-4 border border-black w-full" placeholder='Apartment, suite, etc... (optional)' type="text" id='addressExtra' value={formik.values.addressExtra} onBlur={formik.handleBlur} onChange={formik.handleChange} />
         <input className="p-4 border border-black w-full" placeholder='City' type="text" id='city' value={formik.values.city} onBlur={formik.handleBlur} onChange={formik.handleChange} />
         <div className='flex gap-8'>
-          <input className="p-4 border border-black w-full" placeholder='Location' type="text" />
+          {/* <input className="p-4 border border-black w-full" placeholder='Location' type="text" /> */}
+          <select
+              className="mt-2 w-full rounded border-2 py-2 pl-4 pr-8 font-normal sm:text-xs"
+              type="text"
+              id="location"
+              name="location"
+              placeholder="Location"
+              value={formik.values.location}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            >
+              <option value="" disabled>
+                Select Country
+              </option>
+                <option value='Lithuania'>
+                Lithuania
+                </option>
+                <option value='' disabled>
+                Great Britain (available soon)
+                </option>
+              
+            </select>
         <input className="p-4 border border-black w-full" placeholder='Postal code' type="number" id='postalCode' value={formik.values.postalCode} onBlur={formik.handleBlur} onChange={formik.handleChange} />
         </div>
         <label className='flex'>
-      <input name='keepMeUpdated' className="mr-2 w-6 h-6 border-2 border-gray appearance-none checked:bg-black" type="checkbox" />
+      <input value={formik.values.saveInformation} name='saveInformation' className="mr-2 w-6 h-6 border-2 border-gray appearance-none checked:bg-black" type="checkbox" />
       <span className=' text-lg text-gray-500'>Save this information for next time</span>
       </label>
       </div>
