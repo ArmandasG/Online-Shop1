9d0294f6acbf83446420440e30713a8795901c15
Armandas G <genysarmandas@gmail.com>
2023-08-16 15:54:13 +0300

    css fix

--- src/components/summary/Directory.jsx
+++ src/components/summary/Directory.jsx
@@ -9,7 +9,7 @@ function Directory() {
   return (
     
     <ul className='flex gap-4 container bg-white pt-10'>
-        {elementsToDisplay.map((iObj, index) => (<li className='flex gap-4' key={iObj}><p className={`${iObj.toLowerCase().includes(selectedPart) ? 'font-bold' : ''} text-2xl`}>{iObj}</p><div className={`w-${index === elementsToDisplay.length - 1 ? 0 : 8} h-1 rounded-full bg-gray-300 mt-3.5 `}></div></li>))}
+        {elementsToDisplay.map((iObj, index) => (<li className='flex gap-4' key={iObj}><p className={`${iObj.toLowerCase().includes(selectedPart) ? 'font-bold' : ''} text-2xl`}>{iObj}</p><div className={`${index === elementsToDisplay.length - 1 ? 'w-0' : 'w-8'} h-1 rounded-full bg-gray-300 mt-3.5 `}></div></li>))}
     </ul>
   )
 }
