404a38af68bcd86a1ecb439cbd8fb0cb0dadc083
Armandas G <genysarmandas@gmail.com>
2023-08-23 13:45:10 +0300

    css fixes

--- src/pages/SingleClothesPage.jsx
+++ src/pages/SingleClothesPage.jsx
@@ -24,7 +24,7 @@ function addToCart(items) {
 
 
   return (
-    <div className="min-h-screen text-3xl space-y-10 container">
+    <div className="min-h-screen text-3xl space-y-10 container flex flex-col">
       <img
         src={currentClothesObj.img}
         alt={currentClothesObj.brand}
