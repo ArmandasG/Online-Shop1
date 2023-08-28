c0874e9aa1a9ca1a4e435c4d724351bbf20bf11b
Armandas G <genysarmandas@gmail.com>
2023-08-28 16:25:07 +0300

    Register firebase finished

--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import { useFormik } from "formik";
+import React from "react";
+import * as Yup from "yup";
+import PropTypes from "prop-types";
 
-function RegisterForm() {
+function RegisterForm({ onRegister }) {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Has to be an email example: email@email.com")
+        .required(),
+      password: Yup.string()
+        .min(6, "At least 6 simbols are required")
+        .required(),
+    }),
+    onSubmit: (values) => {
+      onRegister(values);
+    },
+    validate: (values) => {
+        const errors = {};
+        
+        if(!values.email) {
+          errors.email = 'Email is required'
+        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+          errors.email = "Invalid email format";
+        }
+        if(!values.password) {
+          errors.password = 'Password is required'
+        }
+        return errors
+      }
+  });
   return (
-    <div>RegisterForm</div>
-  )
+    <form className="mt-6 space-y-8 p-10 text-3xl" onSubmit={formik.handleSubmit}>
+      <div className="flex flex-col space-y-8">
+        <div className="w-full">
+          <input
+            className={`${
+              formik.touched.email && formik.errors.email
+                ? "border-red-600 focus-visible:outline-red-600"
+                : ""
+            } border p-3 border-black w-full`}
+            type="email"
+            id="email"
+            value={formik.values.email}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="Email *"
+          />
+          <div className="h-1">
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-600 text-base">
+                {formik.errors.email}
+              </div>
+            ) : null}
+          </div>
+        </div>
+        <div className="w-full">
+          <input
+            className={`${
+              formik.touched.message && formik.errors.message
+                ? "border-red-600 focus-visible:outline-red-600"
+                : ""
+            } border p-3 border-black w-full`}
+            type="text"
+            id="password"
+            value={formik.values.password}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder="Password *"
+          />
+          <div className="h-1">
+            {formik.touched.password && formik.errors.password ? (
+              <div className="text-red-600 text-base">
+                {formik.errors.password}
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+      <button
+        className="tracking-wider font-semibold  border py-4 px-40 bg-black text-white w-full"
+        type="submit"
+      >
+        SIGN UP
+      </button>
+    </form>
+  );
 }
 
-export default RegisterForm
+RegisterForm.propTypes = {
+  onRegister: PropTypes.func,
+};
+
+export default RegisterForm;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import RegisterForm from '../components/forms/RegisterForm'
+import { useItemsCtx } from '../context/ItemsContextProvider';
+import { useAuthCtx } from '../context/AuthProvider';
+import { auth } from '../firebase/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { Link } from 'react-router-dom';
 
 function RegisterPage() {
+    const { navigate } = useItemsCtx()
+  const { setIsLoading, ui } = useAuthCtx();
+  function registerToShop({ email, password }) {
+    ui.showLoading();
+    setIsLoading(true);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setIsLoading(false);
+        ui.showSuccess("User has been successfully registered");
+        navigate('/shops')
+      })
+      .catch((error) => {
+        ui.showError("Registration failed");
+        setIsLoading(false);
+      });
+  }
   return (
-    <div>RegisterPage</div>
+    <div className="min-h-screen container items-center gap-32 flex flex-col">
+        
+      <h1 className="text-4xl mt-40 font-bold">Registration</h1>
+      <RegisterForm onRegister={registerToShop} />
+      <div className='text-center'>
+      <p className='text-3xl'>Already have an account ?</p>
+      <Link className='font-bold text-3xl mt-4 hover:underline block' to={"/login"}>
+            {" "}
+            <span>Click here to login</span>{" "}
+          </Link>
+          </div>
+      
+      </div>
   )
 }
 
