df1b37d0012b320790ca98990ecdda3773774681
Armandas G <genysarmandas@gmail.com>
2023-08-28 16:46:31 +0300

    Moving to responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ export default {
         'contact-us': "url('/icons/Contact-us.png')",
       }
     },
+    screens: {
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
