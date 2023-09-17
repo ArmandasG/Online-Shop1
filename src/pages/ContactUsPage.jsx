c6c8d7e6f38f82c0896ac57d7fb5b4722da91e0b
Armandas G <genysarmandas@gmail.com>
2023-09-17 21:13:00 +0300

    ContactUsPage: Visual Bug Fix

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -2,8 +2,8 @@ import ContactUsForm from "../components/forms/ContactUsForm";
 
 function ContactUsPage() {
   return (
-    <div className="lg:min-h-[140rem] container">
-      <div className="mb-20 mt-10 bg-no-repeat bg-cover w-full h-96 lg:h-[60rem] bg-center bg-[url('/icons/Contact_us.png')]"></div>
+    <div className="container pb-20">
+      <div className="mb-6 mt-10 bg-no-repeat bg-cover w-full h-96 lg:h-[60rem] bg-center bg-[url('/icons/Contact_us.png')]"></div>
       <h2 className="font-medium text-3xl mb-10 lg:text-5xl lg:font-bold lg:pt-[6rem]">
         CONTACT
       </h2>
