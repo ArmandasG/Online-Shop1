86548b2f0fdb6e711eeb7253f2e9fc637f61961c
Armandas G <genysarmandas@gmail.com>
2024-08-15 03:34:36 +0300

    Shoper:  - semanthics and url's update

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -5,7 +5,7 @@ function ThankYou() {
     <div className="flex flex-col items-center container h-screen lg:h-[71rem]">
       <div className="bg-no-repeat bg-cover w-96 h-96 bg-center bg-[url('/icons/thankYou.png')]"></div>
       <h1 className="text-center text-4xl py-4 underline">
-        Thank you for trying my website project
+        Thank you for trying my first ever website project
       </h1>
       <p className="text-2xl text-center">
         You can connect with me via LinkedIn and also check out my Github below
@@ -13,13 +13,13 @@ function ThankYou() {
       <div className="flex gap-8 pt-8">
         <a
           className="w-8 h-8 hover:opacity-50"
-          href="https://www.linkedin.com/in/armandas-genys-b4653a176/"
+          href="https://www.linkedin.com/in/armandas-genys/"
         >
           <i className="fa fa-linkedin-square text-5xl" aria-hidden="true"></i>
         </a>
         <a
           className="w-8 h-8 hover:opacity-50"
-          href="https://github.com/ArmandasG/Online-Shop1"
+          href="https://github.com/ArmandasG/"
         >
           <i className="fa fa-github-square text-5xl" aria-hidden="true"></i>
         </a>
